dbd9b05fa54e14b2166d79467f649cf40398ca7d
Mauricio Gomes <mauriciodevelopmentweb@gmail.com>
2024-01-03 21:17:20 -0300

    add base styled components

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+export function App() {
+  return (
+    <Container>
+      <header>Header</header>
+      <main>
+        <h1>App</h1>
+      </main>
+      <footer>footer</footer>
+    </Container>
+  );
+}
