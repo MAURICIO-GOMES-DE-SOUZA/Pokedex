56b77632b4d2d3e4efd1dbb1aa4236ea2d6c95ad
mauricio gomes <mauriciodevelopmentweb@gmail.com>
2024-02-07 20:20:14 -0300

    add page 404

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,8 +1,13 @@
 import { Container } from "./style";
+import page404 from "../../assets/page404.png";
+import { useNavigate } from "react-router-dom";
 
 export function Page404() {
+  const navigate = useNavigate();
   return (
     <Container>
+      <img src={page404} alt="page404" />
+      <button onClick={() => navigate("/")}>voltar</button>
       <h1>Page 404</h1>
     </Container>
   );
